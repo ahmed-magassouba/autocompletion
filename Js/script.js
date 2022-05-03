e2c12d657ac38bcc74be01c06baba11403ce3435
Ahmed Sékou Magassouba <ahmed.magassouba@laplateforme.io>
2022-05-03 08:43:07 +0200

    pajout travail du week end

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    // Les variables
+  // Les variables
   let input = document.querySelector("input");
   let searchResult = document.querySelector("#searchResult");
 
@@ -10,29 +10,36 @@ document.addEventListener("DOMContentLoaded", function () {
   input.addEventListener("keyup", function () {
 
 
-    let data= new FormData(form);
+    let data = new FormData(form);
     //fetcher les données
     fetch("Models/StateModel.php", {
-         method: "POST",
-         body: data
+      method: "POST",
+      body: data
+    })
+      .then(function (response) {
+        return response.json();
       })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (responseData) {
-          console.log(responseData);
+      .then(function (responseData) {
 
-
+        if (input.value.length > 1) {
           
-        });
+          responseData.forEach(element => {
+            console.log(element.name);
+            searchResult.innerHTML= element.name;
+          });
+          
+
+        }
+
+      });
 
 
   })
 
-//   function load_data(querry) {
-//     if (querry.length > 2) {
-//     } else {
-//       searchResult.innerHTML = "Votre recherche n'a pas abouti";
-//     }
-//   }
+  //   function load_data(querry) {
+  //     if (querry.length > 2) {
+  //     } else {
+  //       searchResult.innerHTML = "Votre recherche n'a pas abouti";
+  //     }
+  //   }
 });
